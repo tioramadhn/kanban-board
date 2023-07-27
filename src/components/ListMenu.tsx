b63d5b5784f4fb2a2cddef263da0c6e157e6659d
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-07-27 11:46:55 +0700

    fix modal setting task item

--- src/components/ListMenu.tsx
+++ src/components/ListMenu.tsx
@@ -3,7 +3,7 @@ import ListItem from "./ListItem";
 
 export default function ListMenu() {
   return (
-    <div className="py-2 w-[50vw] md:w-[320px] rounded-lg flex flex-col shadow absolute top-0 right-0 z-[999] bg-white">
+    <div className="box-menu py-2 w-[50vw] md:w-[320px] rounded-lg flex flex-col shadow absolute bottom-[-20] right-0 z-[999] bg-white">
       {listSettings.map((item, idx) => (
         <ListItem menu={item} key={idx} />
       ))}
